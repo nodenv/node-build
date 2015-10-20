a892fbed80fa943a97163b95e147dfde57b67fde
Jason Karns <jason@karns.name>
2015-10-20 11:06:59 -0400

    don't fetch sha info for files that already exist

--- tools/node_scraper.js
+++ tools/node_scraper.js
@@ -3,11 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var baseUrl = "https://nodejs.org/dist/";
 
-var overwrite = false
-
 exports.versions = function getVersions (options) {
-  overwrite = options.overwrite
-
   https.get("https://nodejs.org/dist/index.json", function(res){
     if( res.statusCode !== 200){
       return console.log('response "' + res.status + '" from http://nodejs.org/dist/')
@@ -20,18 +16,35 @@ exports.versions = function getVersions (options) {
     })
 
     res.on('end', function(){
-      JSON.parse(responseData).map(function(build){
-        return build.version
-      }).forEach(generateNodeFile)
+      JSON.parse(responseData)
+      .map(function(build){
+        var filename = filenameFor(build.version)
+        return {
+          version: build.version,
+          file: {
+            name: filename,
+            exists: fs.existsSync(filename)
+          }
+        }
+      })
+      .filter(function(build){
+        return !build.file.exists || options.overwrite
+      })
+      .forEach(generateNodeFile)
     })
   })
 }
 
 // private
 
-function generateNodeFile( version ){
-  var shaUrl = baseUrl + version + "/";
-  var shaData, shaLine, installLine, parts, filePath;
+function filenameFor(version){
+  return path.join(__dirname, '../share/node-build', version.substring(1))
+}
+
+function generateNodeFile( build ){
+  var version = build.version
+  var shaUrl = baseUrl + version + "/"
+  var shaData, shaLine, installLine, parts, filePath
 
   if(/^(v4)/g.test(version)) {
     shaUrl += "SHASUMS256.txt";
@@ -55,24 +68,17 @@ function generateNodeFile( version ){
     res.on('end', function(){
       shaLine = shaData.match(checksum)
 
-      console.log(version, shaData, checksum)
       if(shaLine && shaLine.length){
         parts = shaLine[0].split('  ')
         //this is gnarly, oh well
         //install_package "node-v0.10.0" "http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz#7321266347dc1c47ed2186e7d61752795ce8a0ef"
         installLine = 'Xinstall_package "node-' + version + '" "' + baseUrl + version + '/' + parts[1] + '#'+parts[0]+'"\n'
         filePath = path.join(__dirname, '../share/node-build', version.substring(1))
-        fs.exists(filePath,function(exists){
-
-          if(!exists || overwrite){
-
-            fs.writeFile(filePath, installLine, function(err){
-              if(err)
-                console.log(err)
-              else
-                console.log( version.substring(1) + ' file writen')
-            })
-          }
+        fs.writeFile(filePath, installLine, function(err){
+          if(err)
+            console.log(err)
+          else
+            console.log( version.substring(1) + ' file writen')
         })
       }
     })
