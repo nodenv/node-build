a75dce9ad9c45cf9932d127de7d4f42b9c8479b7
Jason Karns <jason@karns.name>
2015-10-20 09:45:39 -0400

    formatting

--- tools/node_scraper.js
+++ tools/node_scraper.js
@@ -1,66 +1,7 @@
-var http = require('http'),
-	fs = require('fs'),
-	path = require('path'),
-	baseUrl = "http://nodejs.org/dist/",
-	generateNodeFile = function( version ){
-		var shaUrl = baseUrl + version + "/",
-			shaData,
-			shaLine,
-			installLine,
-			parts,
-			filePath
-
-	        if(/^(v4)/g.test(version)) {
-	          shaUrl += "SHASUMS256.txt"
-	          checksum = /[\da-zA-Z]{64}  node-v[\d]{1,2}\.[\d]{1,2}\.[\d]{1,2}.tar.gz/gi
-	        } else {
-	          shaUrl += "SHASUMS.txt"
-	          checksum = /[\da-zA-Z]{40}  node-v[\d]{1,2}\.[\d]{1,2}\.[\d]{1,2}.tar.gz/gi
-	        }
-
-		http.get(shaUrl, function( res ){
-
-			shaData = ''
-
-			res.on('data', function(data){
-				shaData = shaData + data
-			})
-
-			res.on('end', function(){
-
-                		shaLine = shaData.match(checksum)
-
-				if(shaLine && shaLine.length){
-
-					parts = shaLine[0].split('  ')
-					//this is gnarly, oh well
-					//install_package "node-v0.10.0" "http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz#7321266347dc1c47ed2186e7d61752795ce8a0ef"
-					installLine = 'install_package "node-' + version + '" "' + baseUrl + version + '/' + parts[1] + '#'+parts[0]+'"\n'
-					filePath = path.join(__dirname, '../share/node-build', version.substring(1))
-					fs.exists(filePath,function(exists){
-
-						if(!exists){
-
-							fs.writeFile(filePath, installLine, function(err){
-
-								if(err)
-							        console.log(err)
-							    else
-							        console.log( version.substring(1) + ' file writen')
-
-							})
-
-						}
-
-					})
-
-				}
-
-			})
-
-		})
-
-	}
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var baseUrl = "http://nodejs.org/dist/";
 
 exports.versions = function getVersions () {
   http.get(baseUrl, function(res){
@@ -85,9 +26,54 @@ exports.versions = function getVersions () {
       })
 
     } else {
-
       console.log('response "' + res.status + '" from http://nodejs.org/dist/')
-
     }
+  })
+}
+
+// private
+
+function generateNodeFile( version ){
+  var shaUrl = baseUrl + version + "/";
+  var shaData, shaLine, installLine, parts, filePath;
+
+  if(/^(v4)/g.test(version)) {
+    shaUrl += "SHASUMS256.txt";
+    checksum = /[\da-zA-Z]{64}  node-v[\d]{1,2}\.[\d]{1,2}\.[\d]{1,2}.tar.gz/gi;
+  } else {
+    shaUrl += "SHASUMS.txt";
+    checksum = /[\da-zA-Z]{40}  node-v[\d]{1,2}\.[\d]{1,2}\.[\d]{1,2}.tar.gz/gi;
+  }
+
+  http.get(shaUrl, function(res){
+    shaData = ''
+
+    res.on('data', function(data){
+      shaData = shaData + data
+    });
+
+    res.on('end', function(){
+      shaLine = shaData.match(checksum)
+
+      if(shaLine && shaLine.length){
+        parts = shaLine[0].split('  ')
+        //this is gnarly, oh well
+        //install_package "node-v0.10.0" "http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz#7321266347dc1c47ed2186e7d61752795ce8a0ef"
+        installLine = 'install_package "node-' + version + '" "' + baseUrl + version + '/' + parts[1] + '#'+parts[0]+'"\n'
+        filePath = path.join(__dirname, '../share/node-build', version.substring(1))
+        fs.exists(filePath,function(exists){
+
+          if(!exists){
+
+            fs.writeFile(filePath, installLine, function(err){
+              if(err)
+                console.log(err)
+              else
+                console.log( version.substring(1) + ' file writen')
+            })
+          }
+        })
+      }
+    })
   })
 };
