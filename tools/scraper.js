8c501768c15715bcba144d1af259a2f2c8b139cc
Jason Karns <jason@karns.name>
2015-10-20 13:04:08 -0400

    allow overwriting files

--- tools/scraper.js
+++ tools/scraper.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+var force = process.argv[2] == '-f' || process.argv[2] == '--force';
+
 var nodeVersions = require('./node_scraper.js').versions;
 var iojsVersions = require('./iojs_scraper.js').versions;
 
 console.log('Updating node versions');
-nodeVersions();
+nodeVersions({overwrite: force});
 
 console.log('Updating iojs versions');
 iojsVersions();
