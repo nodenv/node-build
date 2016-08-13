rbenv/ruby-build

Allow specifying custom command line options for `aria2c`, `curl` and `wget`

test/cache.bats
@@ -3,6 +3,7 @@
 load test_helper
 export RUBY_BUILD_SKIP_MIRROR=1
 export RUBY_BUILD_CACHE_PATH="$TMP/cache"
+export RUBY_BUILD_ARIA2_OPTS=
 
 setup() {
   mkdir "$RUBY_BUILD_CACHE_PATH"

test/checksum.bats
@@ -3,6 +3,7 @@
 load test_helper
 export RUBY_BUILD_SKIP_MIRROR=1
 export RUBY_BUILD_CACHE_PATH=
+export RUBY_BUILD_ARIA2_OPTS=
 
 
 @test "package URL without checksum" {

nodenv/node-build

test/cache.bats
@@ -2,6 +2,7 @@
 
 load test_helper
 export NODE_BUILD_CACHE_PATH="$BATS_TMPDIR/cache"
+export NODE_BUILD_ARIA2_OPTS=
 
 setup() {
   mkdir -p "$NODE_BUILD_CACHE_PATH"

test/checksum.bats
@@ -2,6 +2,7 @@
 
 load test_helper
 export NODE_BUILD_CACHE_PATH=
+export NODE_BUILD_ARIA2_OPTS=
 
 
 @test "package URL without checksum" {